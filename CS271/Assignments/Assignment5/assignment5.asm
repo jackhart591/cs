7fefe31cd19f71d511ef17966bcc066dc4573152
Endersky78 <jackhart591@gmail.com>
2022-02-28 01:15:02 -0800

    Finished Display proc for assignment 5

--- CS271/Assignments/Assignment5/assignment5.asm
+++ CS271/Assignments/Assignment5/assignment5.asm
@@ -12,34 +12,56 @@ COMMENT &
 
 INCLUDE Irvine32.inc
 
-RAND_LOW	EQU		100
-RAND_HI		EQU		999
-USER_LOW	EQU		15
-USER_HI		EQU		200
+RAND_LOW		EQU		100
+RAND_HI			EQU		999
+USER_LOW		EQU		15
+USER_HI			EQU		200
 
-PARAM_A		EQU		[ebp+8]
+PARAM_A			EQU		[ebp+8]
+PARAM_B			EQU		[ebp+12]
+PARAM_C			EQU		[ebp+16]
+
+LOCAL_A			EQU		[ebp-4]
+LOCAL_B			EQU		[ebp-8]
+
+SPACE_CHAR		EQU		20h
+NUM_PER_LINE	EQU		10
 
 .data
 
-	title_str		BYTE		"Sorting Random Integers", 0
-	author_str		BYTE		"By Jackson Hart", 0
-	intro_str		BYTE		"This program generates random numbers in the range [100 ... 999],", 10, \
-								"displays the original list, sorts the list, and calculates the median value.", 10, \
+	intro_str		BYTE		"Sorting Random Integers", 10, 13, \
+								"By Jackson Hart", 10, 13, \
+								"This program generates random numbers in the range [100 ... 999],", 10, 13, \
+								"displays the original list, sorts the list, and calculates the median value.", 10, 13, \
 								"Finally, it displays the list sorted in descending order.", 0
 	number_prompt	BYTE		"How many numbers should be generated? [15 ... 200]: ", 0
 	invalid_str		BYTE		"Invalid input, try again!", 0
 
-	num_ints		DWORD		?
+	num_ints		DWORD		0
+	num_arr			DWORD		USER_HI DUP(?)
 
 .code
 main PROC
-
+	
+	push	OFFSET intro_str
 	call	Intro
-	push	num_ints
-	call	GetData
-	pop		num_ints
 
-	exit	; exit to operating system
+	push	OFFSET invalid_str
+	push	OFFSET number_prompt
+	push	OFFSET num_ints
+	call	GetData
+
+	mov		eax, num_ints
+	push	num_ints
+	push	OFFSET num_arr
+	call	FillArray
+
+	push	OFFSET num_arr
+	push	num_ints
+	call	DisplayList
+
+
+ 	exit										; exit to operating system
 main ENDP
 
 COMMENT &
@@ -51,32 +73,28 @@ COMMENT &
 	Registers Changed: none
 &
 Intro PROC
+	push	ebp
+	mov		ebp, esp
 	push	edx
 
-	mov		edx, OFFSET title_str
-	call	WriteString
-	call	Crlf
-
-	mov		edx, OFFSET author_str
-	call	WriteString
-	call	Crlf
-
-	mov		edx, OFFSET intro_str
+	mov		edx, PARAM_A
 	call	WriteString
 	call	Crlf
 
 	pop		edx
-	ret
+	pop		ebp
+	ret		4
 Intro ENDP
 
 GetData PROC
 	push	ebp
 	mov		ebp, esp
-	push	edx
 	push	eax
+	push	edx
+	push	esi
 
 Top_A:
-	mov		edx, OFFSET number_prompt
+	mov		edx, PARAM_B
 	call	WriteString
 	call	ReadDec
 
@@ -87,23 +105,48 @@ Top_A:
 	jmp		ValidInput
 
 NotValid:
-	mov		edx, OFFSET invalid_str
+	mov		edx, PARAM_C
 	call	WriteString
 	call	Crlf
 	jmp		Top_A
 
 ValidInput:
-	mov		PARAM_A, eax
+	mov		esi, PARAM_A
+	mov		[esi], eax
 	
-	pop		eax
+	pop		esi
 	pop		edx
+	pop		eax
 	pop		ebp
-	ret
+	ret		12
 GetData ENDP
 
 FillArray PROC
+	push	ebp
+	mov		ebp, esp
+	push	eax
+	push	ecx
+	push	esi
 
-	ret
+	call	Randomize
+	mov		ecx, PARAM_B
+	mov		esi, PARAM_A
+	
+Loop_A:
+	mov		eax, RAND_HI
+	sub		eax, RAND_LOW
+	call	RandomRange
+	inc		eax
+	add		eax, RAND_LOW
+	mov		[esi], eax
+	add		esi, 4
+	loop	Loop_A
+
+	pop		esi
+	pop		ecx
+	pop		eax
+	pop		ebp
+	ret		8
 FillArray ENDP
 
 ExchangeElems PROC
@@ -122,8 +165,48 @@ DisplayMedium PROC
 DisplayMedium ENDP
 
 DisplayList PROC
+	push	ebp
+	mov		ebp, esp
+	sub		esp, 4							; Save space for local vars
+	push	ecx
+	push	esi
+	push	eax
+	push	ebx
 
-	ret
+	mov		ecx, PARAM_A					; ECX -> length of array
+	mov		esi, PARAM_B					; ESI -> base pointer to array
+	mov		DWORD PTR LOCAL_A, SPACE_CHAR
+	mov		edx, ebp
+	sub		edx, 4
+	cld
+
+Loop_B:
+	LODSD
+	call	WriteDec
+	mov		eax, PARAM_A
+	sub		eax, ecx
+	inc		eax
+	push	edx
+	mov		edx, 0
+	mov		ebx, NUM_PER_LINE
+	div		ebx
+	cmp		edx, 0
+	pop		edx
+	jne		Continue
+	call	Crlf
+	jmp		Loop_C
+Continue:
+ 	call	WriteString
+Loop_C:
+	loop	Loop_B
+
+	pop		ebx
+	pop		eax
+	pop		esi
+	pop		ecx
+	mov		esp, ebp
+	pop		ebp
+	ret		8
 DisplayList ENDP
 
 
