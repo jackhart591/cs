ea4fdb25434b8385399b0d73934205a7f436bd05
Endersky78 <jackhart591@gmail.com>
2022-04-12 13:41:39 -0700

    Set up c compiler in activity 2 and finished homework 1 code

---
+++ CS 325/Assignments/Homework1/merge3Time.py
@@ -0,0 +1,24 @@
+import random
+import time
+from mergesort3 import mergesort3
+
+def createlist(n):
+    Data = [None] * n
+    for i in range(n):
+        Data[i] = random.randint(0, 10000)
+    return Data
+
+
+def main():
+    
+    for i in range(1, 11):
+        list = createlist(5000 * i)
+        past = time.time()
+        mergesort3(list)
+        current = time.time()
+        print(f"Time for list with {len(list)} elements:\t{current - past}")
+
+
+
+if __name__ == "__main__":
+    main()
--- CS 325/Assignments/Homework1/mergesort3.py
+++ CS 325/Assignments/Homework1/mergesort3.py
@@ -16,10 +16,10 @@ def merge(Data, Bot, Mid, Top):
         Top = []
     
     while i < len(Bot) and j < len(Mid) and k < len(Top):
-        if (Bot[i] < Mid[j] and Bot[i] < Top[k]):
+        if (Bot[i] <= Mid[j] and Bot[i] <= Top[k]):
             Data[idx] = Bot[i]
             i += 1
-        elif (Mid[j] < Top[k] and Mid[j] < Bot[i]):
+        elif (Mid[j] <= Top[k] and Mid[j] <= Bot[i]):
             Data[idx] = Mid[j]
             j += 1
         else:
@@ -29,7 +29,7 @@ def merge(Data, Bot, Mid, Top):
 
     # If there's any remaining elements
     while (i < len(Bot) and j < len(Mid)):
-        if (Bot[i] < Mid[j]):
+        if (Bot[i] <= Mid[j]):
             Data[idx] = Bot[i]
             i += 1
         else:
@@ -38,7 +38,7 @@ def merge(Data, Bot, Mid, Top):
         idx += 1
 
     while (k < len(Top) and j < len(Mid)):
-        if (Top[k] < Mid[j]):
+        if (Top[k] <= Mid[j]):
             Data[idx] = Top[k]
             k += 1
         else:
@@ -47,7 +47,7 @@ def merge(Data, Bot, Mid, Top):
         idx += 1
 
     while (i < len(Bot) and k < len(Top)):
-        if (Bot[i] < Top[k]):
+        if (Bot[i] <= Top[k]):
             Data[idx] = Bot[i]
             i += 1
         else:
@@ -75,7 +75,7 @@ def merge(Data, Bot, Mid, Top):
 
 def mergesort3(Data):
     if len(Data) > 1:
-        third = len(Data) // 3 # Gets 1/3
+        third = (len(Data) // 3) + 1 # Gets 1/3
 
         Bottom = mergesort3(Data[:third]) # 1/3
         Third = mergesort3(Data[third:(2*third)]) # 2/3
