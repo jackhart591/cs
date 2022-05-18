04e0561c14ada99b0626b752ae30f1b3455042dc
jackhart591 <jackhart591@gmail.com>
2022-05-18 11:28:54 -0700

    Did two demos and added assignment5

---
+++ CS 290/Demos/5-18-22/server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+var port = process.env.PORT || 8000;
+var app = express();
+
+//npm install --save-dev nodemon
+//npm run dev
+//Check package.json to see what I changed
+
+app.use(express.static('public'));
+
+app.get('/people', function (req, res, next) {
+  res.status(200).sendFile(__dirname + '/public/people.html');
+});
+
+var availablePeople = [
+  'luke',
+  'leia',
+  'rey',
+  'finn',
+  'r2d2'
+];
+
+app.get('/people/:person', function (req, res, next) {
+  var person = req.params.person.toLowerCase();
+  if (availablePeople.indexOf(person) >= 0) {
+    res.status(200).sendFile(
+      __dirname + '/public/people/' + person + '.html'
+    );
+  } else {
+    next();
+  }
+});
+
+app.get("*", function (req, res, next) {
+  res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("== Server listening on port", port);
+});
