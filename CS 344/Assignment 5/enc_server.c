6855018be612554a18336bee70e0dced008050ef
Endersky78 <jackhart591@gmail.com>
2022-12-03 15:12:17 -0800

    Fixed errors and started encryption framework

--- CS 344/Assignment 5/enc_server.c
+++ CS 344/Assignment 5/enc_server.c
@@ -10,7 +10,16 @@
 
 void error(const char* msg) {
     perror(msg);
-    exit(0);
+}
+
+void SendMsg(int socket, const char* msg) {
+    int charsSent;
+    charsSent = send(socket,
+                    msg, strlen(msg), 0);
+
+    if (charsSent < 0) {
+        error("ERROR writing to socket");
+    }
 }
 
 void setupAddressStruct(struct sockaddr_in* address, int portNumber) {
@@ -41,16 +50,16 @@ char* recieveFile(int connectionSocket) {
 
     int filesize = atoi(filesizestr);
 
-    printf("filesize: %d\n", filesize);
-
     // Start reading the message
-    printf("Reading\n");
     while (totalRead < filesize) {
         // Get the message from the client and display it
         memset(buffer, '\0', sizeof(buffer));
 
+        // If the remaining bytes is less than size of buffer, don't overread
+        int recvSize = (filesize - totalRead < sizeof(buffer)) ? filesize - totalRead : sizeof(buffer);
+
         // Read the client's message from the socket
-        charsRead = recv(connectionSocket, buffer, sizeof(buffer) - 1, 0);
+        charsRead = recv(connectionSocket, buffer, recvSize, 0);
 
         if (charsRead < 0) {
             error("ERROR reading from the socket");
@@ -63,7 +72,7 @@ char* recieveFile(int connectionSocket) {
             sendStr = malloc(strlen(buffer) + 1);
             sendStr[0] = '\0';
         } else { 
-            sendStr = realloc(sendStr, strlen(sendStr) + sizeof(buffer)); 
+            sendStr = realloc(sendStr, strlen(sendStr) + recvSize + 1); 
         }
 
         strcat(sendStr, buffer);
@@ -71,7 +80,6 @@ char* recieveFile(int connectionSocket) {
 
     } 
 
-    printf("Done'd\n");
     return sendStr;
 }
 
@@ -87,17 +95,18 @@ void childProcess(int connectionSocket) {
     text = recieveFile(connectionSocket);
     key = recieveFile(connectionSocket);
     printf("SERVER: I recieved this from the client: \"%s\"\n", text);
+    printf("\n\nSERVER: I recieved this from the client: \"%s\"\n", key);
     //Send a success message back to the client
-    charsSent = send(connectionSocket,
-                    "I am the server, and I got your message", 39, 0);
+    SendMsg(connectionSocket, "I am the server, and I got your message");
 
-    if (charsSent < 0) {
-        error("ERROR writing to socket");
+    if (strlen(text) <= strlen(key)) {
+        encryptFile(text, key);
+    } else {
+        SendMsg(connectionSocket, "SERVER ERROR: Key's length must be at least as large as the file's length!");
     }
 
-    //handle encryption stuff
-
     free(text);
+    free(key);
     close(connectionSocket);
 }
 
@@ -116,12 +125,8 @@ int main(int argc, char* argv[]) {
     int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (listenSocket < 0) {
         error("ERROR opening socket");
+        exit(1);
     }
-
-    // // Sets the socket as non-blocking
-    // // Code snippet is copied from https://stackoverflow.com/questions/14729007/i-want-to-make-accept-system-call-as-non-blocking-how-can-i-make-accept-system
-    // int flags = fcntl(listenSocket, F_GETFL, 0);
-    // fcntl(listenSocket, F_SETFL, flags | O_NONBLOCK);
 
     // Set up the address struct for the server socket
     setupAddressStruct(&serverAddress, atoi(argv[1]));
@@ -131,6 +136,7 @@ int main(int argc, char* argv[]) {
         (struct sockaddr*) &serverAddress,
         sizeof(serverAddress)) < 0) {
             error("ERROR on binding");
+            exit(1);
         }
 
     // Start listening for connections. Allow up to 5 connections to queue up
@@ -170,11 +176,15 @@ int main(int argc, char* argv[]) {
                             (struct sockaddr*) &clientAddress,
                             &sizeOfClientInfo,
                             SOCK_NONBLOCK);
+
+            if (connectionSocket < 0) {
+                continue;
+            }
         }
 
-        // if (connectionSocket < 0) {
-        //     error("ERROR on accept");
-        // }
+        if (connectionSocket < 0) {
+            error("ERROR on accept");
+        }
 
         pid_t spawnpid = fork();
 
