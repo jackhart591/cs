85318bdcc0e94881a649e1d855b3aded6e97b8a1
Endersky78 <jackhart591@gmail.com>
2023-02-15 10:45:07 -0800

    Finish Homework 4

--- CS 381/HW 4/Stacklang1.hs
+++ CS 381/HW 4/Stacklang1.hs
@@ -1,5 +1,9 @@
 module Stacklang1 where
 
+    -- Name: Jackson Hart
+    -- Date: 2/15/23
+    -- Email: hartjack@oregonstate.edu
+
     type Prog = [Cmd]
     data Cmd = 
         ADD
@@ -12,20 +16,20 @@ module Stacklang1 where
 
     semCmd :: Cmd -> Stack -> Maybe Stack
 
-    semCmd ADD (x:y:xs) = Just ((x+y):xs) 
+    semCmd ADD (x:y:xs) = Just ((x+y):xs) -- if two elems, add them
     semCmd ADD (x) = Nothing
 
-    semCmd DUP (x:xs) = Just (x:x:xs)
+    semCmd DUP (x:xs) = Just (x:x:xs) -- if one elem, dup it
     semCmd DUP [] = Nothing
 
-    semCmd MULT (x:y:xs) = Just (x*y:xs)
+    semCmd MULT (x:y:xs) = Just (x*y:xs) -- if two elems, mult them
     semCmd MULT (x) = Nothing
 
-    semCmd (LD n) s = Just (n:s)
+    semCmd (LD n) s = Just (n:s) -- loads int onto stack
 
     run :: Prog -> Stack -> Maybe Stack
     run [] s = Just s
 
     run (c:cs) s = case semCmd c s of 
         Nothing -> Nothing
-        Just s' -> run cs s'
+        Just s' -> run cs s' -- if there is stuff to run, run it
--- CS 381/HW 4/Stacklang2.hs
+++ CS 381/HW 4/Stacklang2.hs
@@ -1,5 +1,9 @@
 module Stacklang2 where
 
+    -- Name: Jackson Hart
+    -- Date: 2/15/23
+    -- Email: hartjack@oregonstate.edu
+
     type Prog = [Cmd]
 
     type Stack = [Either Bool Int]
@@ -16,35 +20,35 @@ module Stacklang2 where
 
     semCmd ADD [] = Nothing
     semCmd ADD ((Right x):[]) = Nothing
-    semCmd ADD ((Right x):(Right y):xs) = Just ((Right(x+y)):xs)
-    semCmd ADD _ = Nothing
+    semCmd ADD ((Right x):(Right y):xs) = Just ((Right(x+y)):xs) -- if two ints add
+    semCmd ADD _ = Nothing -- else return nothing
 
     semCmd MULT [] = Nothing
     semCmd MULT ((Right x):[]) = Nothing
-    semCmd MULT ((Right x):(Right y):xs) = Just ((Right(x*y)):xs)
-    semCmd MULT _ = Nothing
+    semCmd MULT ((Right x):(Right y):xs) = Just ((Right(x*y)):xs) -- if two ints, mult
+    semCmd MULT _ = Nothing -- else return nothing
 
     semCmd DUP [] = Nothing
-    semCmd DUP (x:xs) = Just (x:x:xs)
+    semCmd DUP (x:xs) = Just (x:x:xs) -- if there is a value to dup, dup it
 
     semCmd (IFELSE _ _) [] = Nothing
     semCmd (IFELSE p p1) ((Left x):xs)
-        | x == True = (run p ((Left x):xs))
-        | x == False = (run p1 ((Left x):xs))
+        | x == True = (run p xs) -- if top of stack is true, run p on the rest of the stack
+        | x == False = (run p1 xs) -- else run p1 on the rest of the stack
 
-    semCmd (LDB b) s = Just ((Left b):s)
-    semCmd (LDI i) s = Just ((Right i):s)
+    semCmd (LDB b) s = Just ((Left b):s) -- Loads a bool onto the stack
+    semCmd (LDI i) s = Just ((Right i):s) -- Loads an int onto the stack
 
     semCmd LEQ [] = Nothing
     semCmd LEQ ((Right x):[]) = Nothing
-    semCmd LEQ ((Right x):(Right y):xs) = Just ((Left(x<=y)):xs)
+    semCmd LEQ ((Right x):(Right y):xs) = Just ((Left(x<=y)):xs) -- Load the result of x<=y if there are two ints
     semCmd LEQ _ = Nothing
 
     run :: Prog -> Stack -> Maybe Stack
     run [] s = Just s
     run (c:cs) s = case semCmd c s of
         Nothing -> Nothing
-        Just s' -> run cs s'
+        Just s' -> run cs s' -- if there is stuff to run, run it
 
     -- stack1 :: Stack
     -- stack1 = [Right 1, Right 3, Right 5, Right 7, Right 9]
