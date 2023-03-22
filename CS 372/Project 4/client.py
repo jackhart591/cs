55e73bae9dbf263a92661c98fa0a7e5f3b9ed921
Endersky78 <jackhart591@gmail.com>
2023-03-21 23:25:57 -0700

    Finished commenting for project 4

--- CS 372/Project 4/client.py
+++ CS 372/Project 4/client.py
@@ -1,22 +1,24 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = 7000
+HOST = "localhost"
+PORT = 8123
 
+# Used echo client/server example from this website:
+# https://realpython.com/python-sockets/#echo-server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
 
     while 1:
-        msg = input("Message to send:\n")
+        msg = input("Message to send:")
         sock.send(msg.encode())
 
         if msg == "/q": # If client is done
             print("Ending chat...")
             break
 
-        recvMsg = sock.recv(1024).decode()
+        recvMsg = sock.recv(1024).decode() # Get message back
 
-        if recvMsg == "/q":
+        if recvMsg == "/q": # If the server is done
             print("Server has ended chat...")
             break
 
--- CS 372/Project 4/server.py
+++ CS 372/Project 4/server.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = 7000
+HOST = "localhost"
+PORT = 8123
 
 # Used echo client/server example from this website:
 # https://realpython.com/python-sockets/#echo-server
@@ -11,15 +11,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     conn, addr = sock.accept()
     with conn:
         while 1:
-            recvMsg = sock.recv(1024).decode()
+            recvMsg = conn.recv(1024).decode() # Look for client message
             print(recvMsg)
 
             if recvMsg == "/q": # If client is done
                 print("Client has ended chat...")
                 break
 
-            msg = input("Message to send:\n")
-            sock.send(msg.encode())
+            msg = input("Message to send:\n") # Send our own message back
+            conn.send(msg.encode())
 
             if msg == "/q": # If server is done
                 print("Ending chat...")
